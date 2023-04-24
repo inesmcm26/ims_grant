3d3ff3d1d4374f3a9dd4bc2b614e8c7f190af3f7
inesmcm <ines.magessi@tecnico.ulisboa.pt>
2023-04-25 00:26:10 +0100

    added accuracy and stdev. changed scaling type

--- script.py
+++ script.py
@@ -52,7 +52,7 @@ from utils import scale
 from models import get_models, generate_configs_DT, generate_configs_RF, generate_configs_GB, generate_configs_AB, generate_configs_SVC, generate_configs_KNN, generate_configs_MLP
 
 # Metrics
-from sklearn.metrics import f1_score, precision_score, recall_score, roc_auc_score
+from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score
 
 # Time
 import time
@@ -68,6 +68,8 @@ seed = 0
 def run(data_path, *args):
     # ------------------------ Read data ------------------------ #
     data = pd.read_csv(data_path)
+
+    data.set_index('PUF_ID', inplace = True)
 
     X = data.drop('fpl', axis = 1)
     y = data['fpl']
@@ -101,14 +103,17 @@ def run(data_path, *args):
         X_train_weights = y_train.map({k: v for k, v in weights.items()})
         X_val_weights = y_val.map({k: v for k, v in weights.items()})
 
-        # --------------------- Missing Values --------------------- #
-
         # -------------- Split numerical and nominal -------------- #
-        X_train_num = X_train[list(filter(lambda x: X_train[x].nunique() > 2, X_train.columns.values))]
-        X_train_cat = X_train[list(filter(lambda x: X_train[x].nunique() == 2, X_train.columns.values))]
+        
+        cat_feats = list(X_train.columns[X_train.nunique() == 2])
+        num_feats = list(X_train.columns[X_train.nunique() > 2])
 
-        X_val_num = X_val[list(filter(lambda x: X_val[x].nunique() > 2, X_val.columns.values))]
-        X_val_cat = X_val[list(filter(lambda x: X_val[x].nunique() == 2, X_val.columns.values))]
+
+        X_train_num = X_train[num_feats]
+        X_train_cat = X_train[cat_feats]
+
+        X_val_num = X_val[num_feats]
+        X_val_cat = X_val[cat_feats]
 
         # ------------------------ Scaling ------------------------ #
         X_train_num, X_val_num = scale(X_train_num, X_val_num)
@@ -118,7 +123,6 @@ def run(data_path, *args):
         X_val = pd.concat([X_val_num, X_val_cat], axis = 1)
 
         # --------------------- Generate Models ------------------- #
-        # models = get_models(class_weights = weights, seed = seed)
         models = get_models(*args, class_weights = weights, seed = seed)
 
         # --------------------- Train and Evaluate ------------------- #
@@ -126,14 +130,22 @@ def run(data_path, *args):
         # If in the first split, initialize the results dict
         if i == 0:
             for model_name in models.keys():
-                results[model_name] = {'Train F1': [],
+                results[model_name] = {'Train Accuracy': [],
+                                       'Train Accuracy Stdev': 0,
+                                       'Train F1': [],
+                                       'Train F1 Stdev': 0,
                                        'Train Precision': [],
+                                       'Train Precision Stdev': 0,
                                        'Train Recall': [],
-                                       'Train ROC AUC': [],
+                                       'Train Recall Stdev': 0,
+                                       'Val Accuracy': [],
+                                       'Val Accuracy Stdev': 0,
                                        'Val F1': [],
+                                       'Val F1 Stdev': 0,
                                        'Val Precision': [],
+                                       'Val Precision Stdev': 0,
                                        'Val Recall': [],
-                                       'Val ROC AUC': []}
+                                       'Val Recall Stdev': 0}
                 
                 
         
@@ -156,36 +168,41 @@ def run(data_path, *args):
             y_val_pred = model.predict(X_val)
 
             # --------------------- Save Results ------------------- #
+            results[model_name]['Train Accuracy'].append(accuracy_score(y_train, y_train_pred, sample_weight = X_train_weights))
             results[model_name]['Train F1'].append(f1_score(y_train, y_train_pred, average = 'weighted', sample_weight = X_train_weights))
             results[model_name]['Train Precision'].append(precision_score(y_train, y_train_pred, average = 'weighted', sample_weight = X_train_weights))
             results[model_name]['Train Recall'].append(recall_score(y_train, y_train_pred, average = 'weighted', sample_weight = X_train_weights))
-            # TODO: dá erro por causa do formato. Dúvida: é mesmo relevante incluir ??
-            # results[model_name]['Train ROC AUC'].append(roc_auc_score(y_train, y_train_pred, average = 'weighted', multi_class = 'ovo', sample_weight = X_train_weights))
 
+            results[model_name]['Val Accuracy'].append(accuracy_score(y_val, y_val_pred, sample_weight = X_val_weights))
             results[model_name]['Val F1'].append(f1_score(y_val, y_val_pred, average = 'weighted', sample_weight = X_val_weights))
             results[model_name]['Val Precision'].append(precision_score(y_val, y_val_pred, average = 'weighted', sample_weight = X_val_weights))
             results[model_name]['Val Recall'].append(recall_score(y_val, y_val_pred, average = 'weighted', sample_weight = X_val_weights))
-            # results[model_name]['Val ROC AUC'].append(roc_auc_score(y_val, y_val_pred, average = 'weighted', multi_class = 'ovo', sample_weight = X_val_weights))
 
 
     for model_name in models.keys():
+        results[model_name]['Train Accuracy Stdev'] = np.std(results[model_name]['Train Accuracy'])
+        results[model_name]['Train Accuracy'] = np.mean(results[model_name]['Train Accuracy'])
+        results[model_name]['Train F1 Stdev'] = np.std(results[model_name]['Train F1'])
         results[model_name]['Train F1'] = np.mean(results[model_name]['Train F1'])
+        results[model_name]['Train Precision Stdev'] = np.std(results[model_name]['Train Precision'])
         results[model_name]['Train Precision'] = np.mean(results[model_name]['Train Precision'])
+        results[model_name]['Train Recall Stdev'] = np.std(results[model_name]['Train Recall'])
         results[model_name]['Train Recall'] = np.mean(results[model_name]['Train Recall'])
-        # results[model_name]['Train ROC AUC'] = np.mean(results[model_name]['Train ROC AUC'])
 
+        results[model_name]['Val Accuracy Stdev'] = np.std(results[model_name]['Val Accuracy'])
+        results[model_name]['Val Accuracy'] = np.mean(results[model_name]['Val Accuracy'])
+        results[model_name]['Val F1 Stdev'] = np.std(results[model_name]['Val F1'])
         results[model_name]['Val F1'] = np.mean(results[model_name]['Val F1'])
+        results[model_name]['Val Precision Stdev'] = np.std(results[model_name]['Val Precision'])
         results[model_name]['Val Precision'] = np.mean(results[model_name]['Val Precision'])
+        results[model_name]['Val Recall Stdev'] = np.std(results[model_name]['Val Recall'])
         results[model_name]['Val Recall'] = np.mean(results[model_name]['Val Recall'])
-        # results[model_name]['Val ROC AUC'] = np.mean(results[model_name]['Val ROC AUC'])
         
-        # TODO: Incluir variância ? SIM
-
     return results
 
 # Generate configurations to be tested
-configs_dt = generate_configs_DT(n_models = 1)
-configs_rf = generate_configs_RF(n_models = 1)
+configs_dt = generate_configs_DT(n_models = 5)
+configs_rf = generate_configs_RF(n_models = 3)
 configs_gb = generate_configs_GB(n_models = 150)
 configs_ab = generate_configs_AB(n_models = 15)
 configs_svc = generate_configs_SVC(n_models = 21)
--- utils.py
+++ utils.py
@@ -1,14 +1,28 @@
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import pandas as pd
 
 def scale(X_train_num, X_val_num):
+    # Use StandardScaler for gaussian features
+    gaussian_feats = ['PPEDUC','DISTRESS','MATERIALISM_1','MATERIALISM_2','ASK1_2','FSscore','FWBscore']
+
     scaler = StandardScaler()
-
     # Fit scaler to training data
-    scaler.fit(X_train_num)
-
+    scaler.fit(X_train_num[gaussian_feats])
     # Scale training and validation data
-    X_train_num = pd.DataFrame(scaler.transform(X_train_num), columns = X_train_num.columns, index = X_train_num.index)
-    X_val_num = pd.DataFrame(scaler.transform(X_val_num), columns = X_val_num.columns, index = X_val_num.index)
+    gaussian_scaled_train = pd.DataFrame(scaler.transform(X_train_num[gaussian_feats]), columns = gaussian_feats, index = X_train_num.index)
+    gaussian_scaled_val = pd.DataFrame(scaler.transform(X_val_num[gaussian_feats]), columns = gaussian_feats, index = X_val_num.index)
+
+    # Use MinMaxScaler for non-gaussian features
+    non_gaussian = list(set(X_train_num.columns) - set(['PPEDUC','DISTRESS','MATERIALISM_1','MATERIALISM_2','ASK1_2','FSscore','FWBscore']))
+
+    scaler = MinMaxScaler()
+    # Fit scaler to training data
+    scaler.fit(X_train_num[non_gaussian])
+    # Scale training and validation data
+    non_gaussian_scaled_train = pd.DataFrame(scaler.transform(X_train_num[non_gaussian]), columns = non_gaussian, index = X_train_num.index)
+    non_gaussian_scaled_val = pd.DataFrame(scaler.transform(X_val_num[non_gaussian]), columns = non_gaussian, index = X_val_num.index)
+
+    X_train_num = pd.concat([gaussian_scaled_train, non_gaussian_scaled_train], axis = 1)
+    X_val_num = pd.concat([gaussian_scaled_val, non_gaussian_scaled_val], axis = 1)
 
     return X_train_num, X_val_num
